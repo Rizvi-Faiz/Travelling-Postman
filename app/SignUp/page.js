e953e4f3a9d8ddcef73eb176eb6dfa6183fb0261
kesharwaniyanshi <kesharwaniyanshi@gmail.com>
2024-12-12 02:48:05 +0530

    signup done

--- app/SignUp/page.js
+++ app/SignUp/page.js
@@ -366,7 +366,7 @@ export default function SignUp() {
 
       <Header />
 
-      <div className="w-full max-w-3xl bg-white bg-opacity-90 p-8 rounded-xl shadow-2xl mt-8">
+      <div className="w-full max-w-4xl bg-white bg-opacity-90 p-8 rounded-xl shadow-2xl mt-8">
 
 
         <h1 className="text-3xl font-bold text-center text-red-700 mb-2">Welcome!</h1>
@@ -381,7 +381,8 @@ export default function SignUp() {
           <div className="w-full md:w-1/2 p-4">
 
             {/* form */}
-            <div className="mb-3">
+            <div className="w-full flex flex-row gap-1">
+            <div className="w-1/2 mb-3">
               <label htmlFor="role" className="block text-md mb-2 font-semibold">
                 Choose Role
               </label>
@@ -400,7 +401,7 @@ export default function SignUp() {
               </select>
             </div>
 
-            <div className="mb-3">
+            <div className="w-1/2 mb-3">
               <label htmlFor="name" className="block text-md mb-2 font-semibold">
                 Full Name
               </label>
@@ -413,8 +414,11 @@ export default function SignUp() {
                 className="border border-gray-300 rounded px-4 py-2 w-full focus:ring-2 focus:ring-red-500 focus:outline-none"
               />
             </div>
+            </div>
 
-            <div className="mb-3">
+
+            <div className="w-full flex flex-row gap-1">
+            <div className="w-1/2 mb-3">
               <label htmlFor="email" className="block text-md mb-2 font-semibold">
                 Email
               </label>
@@ -428,7 +432,7 @@ export default function SignUp() {
               />
             </div>
 
-            <div className="mb-3">
+            <div className="w-1/2 mb-3">
               <label htmlFor="contact" className="block text-md mb-2 font-semibold">
                 Contact Number
               </label>
@@ -441,7 +445,7 @@ export default function SignUp() {
                 className="border border-gray-300 rounded px-4 py-2 w-full focus:ring-2 focus:ring-red-500 focus:outline-none"
               />
             </div>
-
+            </div>
             {role === "User" && (
               <div className="mb-">
                 <label htmlFor="address" className="block text-md mb-2 font-semibold">
@@ -495,7 +499,8 @@ export default function SignUp() {
               />
             </div>
 
-            <div className="mb-3">
+            <div className="w-full flex flex-row gap-1">
+            <div className="w-1/2 mb-3">
               <label htmlFor="password" className="block text-md mb-2 font-semibold">
                 Password
               </label>
@@ -509,7 +514,7 @@ export default function SignUp() {
               />
             </div>
 
-            <div className="mb-3">
+            <div className="w-1/2 mb-3">
               <label htmlFor="confirmPassword" className="block text-md mb-2 font-semibold">
                 Confirm Password
               </label>
@@ -522,7 +527,7 @@ export default function SignUp() {
                 className="border border-gray-300 rounded px-4 py-2 w-full focus:ring-2 focus:ring-red-500 focus:outline-none"
               />
             </div>
-
+            </div>
             <div className="flex justify-center space-x-4 mb-3">
               <button
                 onClick={handleSignUp}
